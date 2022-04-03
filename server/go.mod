81fe56af453a51723b60d28f473fc1cec08c0b7f
DaisukeMatsumoto0925 <daisuke.matsumoto0925@gmail.com>
2022-04-03 14:41:24 +0900

    setup

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/DaisukeMatsumoto0925/auth0_sample
+
+go 1.18
---
+++ server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	v1 "github.com/DaisukeMatsumoto0925/auth0_sample/handlers/v1"
+)
+
+const (
+	port = 8000
+)
+
+func main() {
+	mux := http.NewServeMux()
+	// /v1へのリクエストが来た場合のハンドラを追加
+	mux.HandleFunc("/v1", v1.HandleIndex)
+
+	addr := fmt.Sprintf(":%d", port)
+
+	// localhost:8000 でサーバーを立ち上げる
+	log.Printf("Listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
